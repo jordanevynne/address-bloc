5900482e73b994e9cd51894f99b0e75be7aea72a
Jordan Marcus <jordan.e.marcus@gmail.com>
2016-02-22 21:05:20 -0800

    Checkpoint 23 interfaces: completed

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -2,20 +2,39 @@ require_relative '../models/entry'
 
 RSpec.describe Entry do
   describe "attributes" do
-    it "responds to name" do
-      entry = Entry.new
+  let(:entry) { Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com') }
 
+  describe "#to_s" do
+    it "prints an entry as a string" do
+      entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+
+      expect(entry.to_s).to eq(expected_string)
+    end
+  end
+
+    it "responds to name" do
       expect(entry).to respond_to(:name)
     end
 
-    it "responds to phone number" do
-      entry = Entry.new
+    it "reports its name" do
+      expect(entry.name).to eq('Ada Lovelace')
+    end
+
+    it "reports its phone_number" do
+      expect(entry.phone_number).to eq('010.012.1815')
+    end
+
+    it "responds to phone_number" do
       expect(entry).to respond_to(:phone_number)
     end
 
     it "responds to email" do
-        entry = Entry.new
-        expect(entry).to respond_to(:email)
+      expect(entry).to respond_to(:email)
+    end
+
+    it "reports its email" do
+      expect(entry.email).to eq('augusta.king@lovelace.com')
     end
   end
 end
